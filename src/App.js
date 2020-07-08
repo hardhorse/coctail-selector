dcdded395bafd30b4a788b27f890da7304c4e2c5
Vyacheslav Vaschenok <byslavik@gmail.com>
2020-07-08 23:17:57 +0300

    first build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
 import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+import CaliforniaImg from './img/california.png';
+import ChistyKaifImg from './img/chisty-kaif.png';
+import HodimPoKrayImg from './img/hodim-po-kray.png';
+import LubovImg from './img/lubov.png';
+import MoyaVselennayaImg from './img/moya-vselennaya.png';
+import SolncaNeVidno from './img/solnca-ne-vidno.png';
+
+import MainImg from './img/main.png';
+
+import {questions, COCTAILS} from './questions';
+
 import './App.css';
 
+const imageMapping = {
+  [COCTAILS.california]: CaliforniaImg,
+  [COCTAILS.chistyKaif]: ChistyKaifImg,
+  [COCTAILS.hodimPoKrau]: HodimPoKrayImg,
+  [COCTAILS.lubovBexPamyati]: LubovImg,
+  [COCTAILS.moyaVselennaya]: MoyaVselennayaImg,
+  [COCTAILS.solncaNeVidno]: SolncaNeVidno,
+}
+
+const QuestionCard = ({
+  question,
+  options,
+  index,
+  onSubmit,
+  isLastQuestion
+}) => {
+const [value, setValue] = React.useState('');
+
+const handleChange = (event) => {
+  setValue(event.target.value);
+};
+return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography style={{marginBottom: 20}} variant="h5">{question}</Typography>
+        <FormControl component="fieldset" fullWidth>
+          <RadioGroup name={`question${index}`} value={value} onChange={handleChange}>
+            {options.map((item, index) => <FormControlLabel style={{lineHeight: 1.2, marginBottom: 10}} key={index} value={item.key} control={<Radio />} label={item.label} />)}
+          </RadioGroup>
+          <Button disabled={!value} onClick={() => {
+            onSubmit(value);
+            setValue('')
+          }} fullWidth style={{marginTop: 20}} variant="outlined" color="primary">
+            { isLastQuestion ? 'Получить коктейль' : 'Давай дальше!' }
+          </Button>
+        </FormControl>
+      </CardContent>
+    </Card>
+  )
+}
+
+const STATUSES = {
+  init: 'init',
+  question: 'question',
+  result: 'result'
+}
+
+const getOftenElement = arr => {
+  const b=[];
+  let max='', maxi=0;
+  for(var k in arr) {
+      b[k] ? b[k]++ : b[k]=1;
+      if(maxi<b[k]) { max=k; maxi=b[k] }
+  }
+
+  return maxi
+}
+
 function App() {
+  const questionNumber = questions.length;
+  const values = React.useRef([]);
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [status, setStatus] = React.useState(STATUSES.init);
+
+  const handleNextSlide = (value) => {
+    values.current.push(value);
+    if (currentQuestion === questionNumber -1) {
+      setStatus(STATUSES.result);
+      return;
+    }
+    setCurrentQuestion(currentQuestion + 1);
+  }
+
+  const isLastQuestion = currentQuestion === questionNumber -1;
+
+  const handleReset = () => {
+    values.current = [];
+    setCurrentQuestion(0);
+    setStatus(STATUSES.init);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <CssBaseline />
+      <Container style={{paddingTop: 20}} maxWidth="lg">
+        {
+          status === STATUSES.question &&
+            <QuestionCard isLastQuestion={isLastQuestion} index={currentQuestion} onSubmit={handleNextSlide} {...questions[currentQuestion]}/>
+        }
+        {
+          status === STATUSES.init &&
+            <>
+              <img className="imgWithBounceAnimation" style={{width: '100%'}} src={MainImg} alt="main-img"/>
+              <Button onClick={() => setStatus(STATUSES.question)} fullWidth style={{marginTop: 20}} variant="outlined" color="primary">
+                Я хочу коктейль!
+              </Button>
+            </>
+        }
+        {
+          status === STATUSES.result && <>
+            <Typography align="center" variant="h5">Супер! Ваш коктейль:</Typography>
+            <img className={'imgWithUpDownAnimation'} alt="Коктейль" src={imageMapping[values.current[getOftenElement(values.current)]]} />
+
+            <Button onClick={handleReset} fullWidth variant="outlined" color="primary">
+                Давай сначала!
+              </Button>
+          </>
+        }
+      </Container>
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,158 @@
+export const COCTAILS = {
+    california: 'Калифорния',
+    hodimPoKrau: 'Ходим по краю',
+    lubovBexPamyati: 'Любовь без памяти',
+    solncaNeVidno: 'Солнца не видно',
+    chistyKaif: 'Чистый кайф',
+    moyaVselennaya: 'Моя вселенная',
+};
+
+
+export const questions = [
+    {
+        question: "Какое описание больше всего подходит?",
+        options: [
+            {
+                key: COCTAILS.california,
+                label: "Я сладкий/ая, т.к. меня уронили в детстве в сироп"
+            },
+            {
+                key: COCTAILS.hodimPoKrau,
+                label: "Я не страдаю безумием... Я им наслаждаюсь. "
+            },
+            {
+                key: COCTAILS.lubovBexPamyati,
+                label: "Когда красивым налево, а умным направо, мне хоть разорвись"
+            },
+            {
+                key: COCTAILS.solncaNeVidno,
+                label: "He люблю я хамов. Зачем мне конкyренты?"
+            },
+            {
+                key: COCTAILS.chistyKaif,
+                label: "Я с вами себя таким/ой умным/ой чувствую."
+            },
+            {
+                key: COCTAILS.moyaVselennaya,
+                label: "Я - как хорошее вино: с годами становлюсь крепче, насыщеннее и ярче."
+            }
+        ]
+    },
+
+    {
+        question: "Ваше идеальное времяприпррвождение?",
+        options: [
+            {
+                key: COCTAILS.chistyKaif,
+                label: "Я бы весь день валялся/лась на пляже!"
+            },
+            {
+                key: COCTAILS.moyaVselennaya,
+                label: "Лучший досуг - проведенный с умом, поэтому почитал/ла бы книгу."
+            },
+            {
+                key: COCTAILS.lubovBexPamyati,
+                label: "Яхта, море и ветер в волосах."
+            },
+            {
+                key: COCTAILS.solncaNeVidno,
+                label: "Прыгнул/ла бы с парашютом. А вообще, любой активный отдых."
+            },
+            {
+                key: COCTAILS.hodimPoKrau,
+                label: "Пойду на рейв."
+            },
+            {
+                key: COCTAILS.california,
+                label: "Время с семьей - это идеально."
+            }
+        ]
+    },
+    {
+        question: "Какую песни вы бы хотели, чтобы DJ поставил именно для вас?",
+        options: [
+            {
+                key: COCTAILS.hodimPoKrau,
+                label: "Моргенштерн - самое оно!"
+            },
+            {
+                key: COCTAILS.chistyKaif,
+                label: "Я бы повальсировал/ла под Моцарта..."
+            },
+            {
+                key: COCTAILS.california,
+                label: "Что-нибудь из репертуара Энрике Иглесиаса..."
+            },
+            {
+                key: COCTAILS.solncaNeVidno,
+                label: "Душа просит Queen!"
+            },
+            {
+                key: COCTAILS.moyaVselennaya,
+                label: "Мне так нравится Синатра!"
+            },
+            {
+                key: COCTAILS.lubovBexPamyati,
+                label: "А можно что-нибудь современное?"
+            }
+        ]
+    },
+    {
+        question: "Как вы планируете закончить вечеринку?",
+        options: [
+            {
+                key: COCTAILS.lubovBexPamyati,
+                label: "На афтепати в бассейне"
+            },
+            {
+                key: COCTAILS.moyaVselennaya,
+                label: "За душевными разговорами"
+            },
+            {
+                key: COCTAILS.hodimPoKrau,
+                label: "Танцуя всю ночь"
+            },
+            {
+                key: COCTAILS.california,
+                label: "С тортиком и кофейком "
+            },
+            {
+                key: COCTAILS.solncaNeVidno,
+                label: "Так, чтобы ничего не помнить на утро"
+            },
+            {
+                key: COCTAILS.chistyKaif,
+                label: "За рулем авто, слушая песни Меладзе по дороге домой"
+            }
+        ]
+    },
+    {
+        question: "С чем у вас ассоциируется Настя?",
+        options: [
+            {
+                key: COCTAILS.lubovBexPamyati,
+                label: "Fashion"
+            },
+            {
+                key: COCTAILS.solncaNeVidno,
+                label: "Ремарк"
+            },
+            {
+                key: COCTAILS.chistyKaif,
+                label: "Бабочки"
+            },
+            {
+                key: COCTAILS.hodimPoKrau,
+                label: "Искусственный интеллект"
+            },
+            {
+                key: COCTAILS.california,
+                label: "Задушевные беседы"
+            },
+            {
+                key: COCTAILS.moyaVselennaya,
+                label: "Искусство"
+            }
+        ]
+    }
+];
